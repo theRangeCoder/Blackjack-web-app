e6a9f6356461f68fd2fb7f4df2b093989d061fd3
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-19 23:46:13 +0200

    Created the two cards, with their sum

---
+++ index.js
@@ -0,0 +1,9 @@
+// 1. Create two variables, firstCard and secondCard
+// Set their values to a random number between 2-11
+let firstCard, secondCard;
+firstCard = Math.floor(2 + Math.random()*(11-2));
+secondCard = Math.floor(2 + Math.random()*(11-2));
+
+// 2. Create a variable, sum, and set it to the sum of the two cards
+let sum = firstCard + secondCard;
+
