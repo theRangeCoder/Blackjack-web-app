78c82d663f873629a7a7d6ee7aeeff43edbad226
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-20 20:29:40 +0200

    Added a variable to keep track of the players' Blackjack status

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-// 1. Create two variables, firstCard and secondCard
-// Set their values to a random number between 2-11
+// Creating two variables, firstCard and secondCard
+// Setting their values to a random number between 2-11
 let firstCard, secondCard;
 firstCard = Math.round(2 + Math.random()*(11-2));
 secondCard = Math.round(2 + Math.random()*(11-2));
 
-// 2. Create a variable, sum, and set it to the sum of the two cards
+// Creating a variable to keep a track if the player has Blackjack or not
+let hasBlackjack = false;
+
+// Creating a variable, sum, and setting it to the sum of the two cards
 let sum = firstCard + secondCard;
 console.log(sum);
 
-// 3. Create a conditional to implement the objective of the game
-if (sum < 21) {
+// Creating a conditional to implement the objective of the game
+if (sum <= 20) {
     console.log("Would you like to draw a new card?")
 } else if (sum === 21) {
     console.log("Congratulations! You've got Blackjack!")
+    hasBlackjack = true;
 } else {
     console.log("Sorry! You're out of the game")
 }
