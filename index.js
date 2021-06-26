f8e4d9998536d5525d2d65d7c893cadd31419c6f
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-06-27 01:29:06 +0200

    Modified the renderGame() and newCard() functions to correct for the new card

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let firstCard, secondCard;
 firstCard = Math.round(2 + Math.random()*(11-2));
 secondCard = Math.round(2 + Math.random()*(11-2));
 
+// Creating an array to store the cards
+let cards = [firstCard, secondCard];
+
 // Creating a variable, hasBlackjack, to keep a track if the player has Blackjack or not
 let hasBlackjack = false;
 
@@ -25,8 +28,12 @@ let sumEl = document.querySelector("#sum-el"); // based on a CSS selector
 
 // Creating a renderGame() function to render the state of the game at every step
 function renderGame() {
-    cardsEl.textContent += " "+firstCard+" "+secondCard;
+    cardsEl.textContent = "Cards: ";
 
+    for (let card of cards) {
+        cardsEl.textContent += card + " ";
+    }
+    
     if (sum <= 20) {
         message = "Would you like to draw a new card?";
     } else if (sum === 21) {
@@ -50,6 +57,9 @@ function newCard() {
 
     // Creating a new variable to assign the value of a new card
     let nCard = Math.round(2 + Math.random()*(11-2));
+
+    // Adding the new card to the cards array
+    cards[cards.length] = nCard;
 
     // Adding the value of the new card to the sum variable
     sum += nCard;
